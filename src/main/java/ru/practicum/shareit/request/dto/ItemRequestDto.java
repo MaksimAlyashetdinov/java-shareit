443f9031ef688e04c3ac219c9e57fea7826c838f
Maksim Alyashetdinov <makc752@yandex.ru>
2023-05-27 21:12:02 +0300

    Доработано с учетом замечаний по итогам ревью

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -4,10 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
 @Data
-public class ItemRequestDto {
+public class ItemRequestDto extends ItemRequest {
 
     private Long id;
     private String description;
--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,11 +3,12 @@ package ru.practicum.shareit.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.User;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
+public class UserDto extends User {
 
     private Long id;
     private String name;
